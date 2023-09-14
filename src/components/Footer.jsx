7671d832fe638ec638a31dd36e54f8375db61523
Justin <dong135790@gmail.com>
2023-09-14 19:32:51 -0400

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ function Footer() {
     <>
       <div id="footerContainer">
         <a className="footerElement" href="https://github.com/dong135790">Github</a>
-        <a className="footerElement" href="">Instagram</a>
-        <a className="footerElement" href="">Linkedin</a>
+        <a className="footerElement" href="https://www.instagram.com/jusstin.dong/">Instagram</a>
+        <a className="footerElement" href="https://www.linkedin.com/in/justin-dong-662a46205">Linkedin</a>
       </div>
     </>
   );
