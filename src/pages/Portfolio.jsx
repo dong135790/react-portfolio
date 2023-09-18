01771e1f58f5c2edc049b77f5a49789667b56574
Justin <dong135790@gmail.com>
2023-09-17 23:23:55 -0400

    added new portfolio, updated link, and profile picture

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,24 +4,31 @@ function Portfolio() {
 
   const [hover, setHover] = useState(false); // initial false
 
-
   const onHover = (e) => {
     e.preventDefault();
-    setHover(true); // turn true
+    let getImage = document.getElementById('portfolioContainer');
+    const image = getImage.children
+    const image2 = getImage.children[1]
+    console.log([getImage], image, image2);
+    setTimeout(() => {
+      setHover(true);
+    }, 50);
     console.log("hovered");
   };
-
+  
   const onHoverOver = (e) => {
     e.preventDefault(); // turn false
-    setHover(false);
+    setTimeout(() => {
+      setHover(false);
+    }, 100);
   };
   return (
     <>
       <h2>Portfolio</h2>
         <div id="portfolioContainer">
           <div className="singlePortfolioItem">
-            <a href="https://github.com/swatson2000/Digital-Museum" rel="noreferrer" target="_blank">
-              {hover && <p className={hover}>{"Digital Museum"}</p>}
+            <a href="https://swatson2000.github.io/Digital-Museum/" rel="noreferrer" target="_blank">
+              {hover && <p>{"Digital Museum"}</p>}
               <img onMouseEnter={(e) => onHover(e)}
                 onMouseLeave={(e) => onHoverOver(e)}
                 className="img-responsive" id="portfolioImg" src="src/assets/museum.png" alt="digital-museum" />
@@ -32,23 +39,39 @@ function Portfolio() {
               {hover && <p className={hover}>{"ReadMe Generator"}</p>}
               <img onMouseEnter={(e) => onHover(e)}
                 onMouseLeave={(e) => onHoverOver(e)} 
-                id="portfolioImg" src="src/assets/readme generator.png" alt="readme-generator-via-express"/>
+                id="portfolioImg" src="src/assets/readme-generator.png" alt="readme-generator-via-express"/>
             </a>
           </div>
           <div className="singlePortfolioItem">
-          <a href="https://github.com/dong135790/swoldemort" rel="noreferrer" target="_blank" >
+          <a href="https://application-2023-07-31-69c69ce85b50.herokuapp.com/login" rel="noreferrer" target="_blank" >
             {hover && <p className={hover}>{"Swoldemort"}</p>}
             <img onMouseEnter={(e) => onHover(e)}
                 onMouseLeave={(e) => onHoverOver(e)}  
-                id="portfolioImg" src="src/assets/Screenshot 2023-09-14 133419.png" alt="swoldemort-workout-routine"/>
+                id="portfolioImg" src="src/assets/swoledemort.png" alt="swoldemort-workout-routine"/>
           </a>
           </div>
           <div className="singlePortfolioItem">
-          <a href="https://github.com/dong135790/pwa-text-editor" rel="noreferrer" target="_blank">
+          <a href="https://damp-thicket-22045-f9b7c3017935.herokuapp.com/" rel="noreferrer" target="_blank">
           {hover && <p className={hover}>{"PWA Text Editor"}</p>}
             <img onMouseEnter={(e) => onHover(e)}
                 onMouseLeave={(e) => onHoverOver(e)}  
-                id="portfolioImg" src="src/assets/text editor.png" alt="pwa-text-generator" />
+                id="portfolioImg" src="src/assets/text-editor.png" alt="pwa-text-generator" />
+          </a>
+          </div>
+          <div className="singlePortfolioItem">
+          <a href="https://dong135790.github.io/weather-forecast-dashboard/" rel="noreferrer" target="_blank">
+          {hover && <p className={hover}>{"Weather Forecast Dashboard"}</p>}
+            <img onMouseEnter={(e) => onHover(e)}
+                onMouseLeave={(e) => onHoverOver(e)}  
+                id="portfolioImg" src="src/assets/weather-forecast.png" alt="weather-forecast-dashboard" />
+          </a>
+          </div>
+          <div className="singlePortfolioItem">
+          <a href="https://github.com/dong135790/employee-tracker" rel="noreferrer" target="_blank">
+          {hover && <p className={hover}>{"Employee Tracker"}</p>}
+            <img onMouseEnter={(e) => onHover(e)}
+                onMouseLeave={(e) => onHoverOver(e)}  
+                id="portfolioImg" src="src/assets/employee-tracker.png" alt="pwa-text-generator" />
           </a>
           </div>
         </div>
