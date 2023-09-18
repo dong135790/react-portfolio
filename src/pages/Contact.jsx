be18ccce9d7ac9f214811f26162b117c8d1222e0
Justin <dong135790@gmail.com>
2023-09-17 23:52:04 -0400

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,11 +13,11 @@ function Contact() {
     <h2>Contact</h2>
     <form>
       <label htmlFor="firstName">First Name:</label><br/>
-      <input type="text" id='firstName' name="firstName"/><br />
+      <input type="text" id='firstName' name="firstName" required maxLength={20}/><br />
       <label htmlFor="email">Email address:</label><br/>
-      <input type="text" id='email' name="email"/><br/>
+      <input type="text" id='email' name="email"required /><br/>
       <label htmlFor="message">Message</label><br/>
-      <input type="text" id='message' name="message"/><br/>
+      <input type="text" id='message' name="message" required minLength={5} maxLength={5000}/><br/>
       <input type="submit" value="Submit" />
     </form>
     </>
